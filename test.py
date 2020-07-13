4e73f55d7f886487aa17c82028efece0010b389d
Lim Cheol Ho <dusk95@naver.com>
2020-07-13 19:34:38 +0900

    코드 수정
    코드 수정

--- test.py
+++ test.py
@@ -16,15 +16,29 @@ from net import Net
 from torchvision.utils import save_image
 import shutil
 import natsort
+from torchvision import models
+#from torchsummary import summary
+from torchvision import datasets, models, transforms
 
-transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),transforms.Resize((32, 32)), transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
-# testset = torchvision.datasets.ImageFolder(root="./test/", transform= transform)
-# classes = testset.classes
-# testloader = DataLoader(testset, batch_size= 4, shuffle=False, num_workers=4)
+path = './test/'#구분할 이미지 파일 폴더 경로 입력(input) #S:/bri/
+output_path = './class/'#구분한 이미지파일 저장할 경로 입력(output)
 
-path = 'D:/bri/'#구분할 이미지 파일 폴더 경로 입력(input) #S:/bri/
-output_path = 'D:/clean/'#구분한 이미지파일 저장할 경로 입력(output)
-pth_file = 'cifar_net.pth'# pth파일 디렉토리 직접입력(weight)
+text = open('./recogo.txt', 'w', encoding='UTF8' )
+
+
+#클래스 분류
+def find_classes(dir):
+    classes = os.listdir(dir)
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    return classes, class_to_idx
+
+
+transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
 
 #===================================================================#
 
@@ -38,7 +52,7 @@ ac = listlist.append #이미지 경로
 copy = copylist.append
 fileappend = file_name.append #이미지 이름
 
-name = 'D:/clean/'
+name = './class/'
 
 for i in path_list: # 왕 이름
     pathA = path + str(i) +'/'
@@ -64,45 +78,41 @@ for i in path_list: # 왕 이름
                 copy(str(pathC_l_C))
                 fileappend(l) #이름만 저장
 
-i=0
-if __name__ == '__main__':
-    
-    files = []
-    net = Net() 
-    net = net.cuda()
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = False
+
+files = []
+
+# r=root, d=directories, f = files           
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.jpg' in file:
+            files.append(os.path.join(r, file))
+dir2 = './img/train/'
+class_name, class_to_idx = find_classes(dir2)
+
+for target_class in sorted(class_to_idx.keys()):
+    class_index = class_to_idx[target_class]
+
+for f in files:
+    img = Image.open(f).convert("RGB")  # Load image as PIL.Image
+    x = transform(img)  # Preprocess image
+    x = x.unsqueeze(0)  # Add batch dimension
+    model = torchvision.models.resnet152(num_classes = 186) # 모델 
+    model.load_state_dict(torch.load('cifar_ResNet1818.pth'))
+    model.eval()
+    output = model(x)  # Forward pass
+    pred = torch.argmax(output, 1)  # Get predicted class if multi-class classification
+        
+    if not os.path.exists(output_path):
+            os.makedirs(output_path)
 
     
-    correct = 0
-    total = 0
+    for i in range(0,len(class_name)):
+        if pred == i:
+            if not os.path.exists(output_path+'/'+str(class_name[i])):
+                os.mkdir(output_path+'/'+str(class_name[i]))
+            shutil.copy( f ,output_path+'/'+str(class_name[i]))
+            print("predicted as " +str(class_name[i]))
+            text.write(str(class_name[i]))
 
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.jpg' in file:
-                files.append(os.path.join(r, file))
 
-    for f in files:
-        img = Image.open(f)  # Load image as PIL.Image
-        x = transform(img)  # Preprocess image
-        x = x.unsqueeze(0)  # Add batch dimension
-        model = torch.load_state_dict(torch.load('./cifar_net_adam.pth'))
-        output = model(x)  # Forward pass
-        pred = torch.argmax(output, 1)  # Get predicted class if multi-class classification
-            
-
-        if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            
-        if pred == 0:
-            if not os.path.isdir(copylist[i]):
-                os.makedirs(copylist[i]+'/')
-            shutil.copy( listlist[i], copylist[i])
-            print("predicted as A01" )
-        elif pred == 1:
-            if not os.path.isdir(copylist[i]):
-                os.makedirs(copylist[i]+'/')
-            shutil.copy( listlist[i],'D:/A02/')
-            print("predicted as A02" )
-        i=i+1
 
--- tf_lean.py
+++ tf_lean.py
@@ -3,56 +3,38 @@ import torch
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import torchvision
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
 import multiprocessing as mp
 import os
 import cv2
+
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
 import torch.optim as optim
-from net import Net
+
+from net import Net # 개인 커스텀 네트워크를 쓰고 싶을때
+
+import torchvision
+from torch.utils.data import Dataset, DataLoader
 from torchvision.utils import save_image
 from torch.optim import lr_scheduler
 from torchvision import datasets, models, transforms
+
 import time
-import os
 import copy
 import natsort
 import shutil
 from adamp import AdamP
-# data_transforms = {
-#     'train': transforms.Compose([transforms.Grayscale(num_output_channels=1),transforms.Resize((32, 32)), 
-#     transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
-# ,
-#     'val': transforms.Compose([transforms.Grayscale(num_output_channels=1),transforms.Resize((32, 32)), 
-#     transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
-# ,}
-
-def find_classes(dir):
-    classes = os.listdir(dir)
-    classes.sort()
-    class_to_idx = {classes[i]: i for i in range(len(classes))}
-    return classes, class_to_idx
-
-dir2 = './img/train/'
-class_name, class_to_idx = find_classes(dir2)
-class_correct = list(0. for i in range(186))
-class_total = list(0. for i in range(186))
-
-for target_class in sorted(class_to_idx.keys()):
-    class_index = class_to_idx[target_class]
 
 data_transforms = {
     'train': transforms.Compose([
         transforms.Resize((228, 228)),
         transforms.RandomRotation(15),
-        transforms.RandomAffine(degrees=0,shear=20, scale=(0.8, 1.2),fillcolor=255),
-        transforms.RandomResizedCrop(224, scale=(0.7, 1.4), ratio=(0.75, 1.3333333333333333)),
+        transforms.RandomAffine(0, shear=20, scale=(0.8, 1.3)),
+        transforms.RandomResizedCrop(224, scale=(0.8, 1.4), ratio=(0.75, 1.3333333333333333)),
         transforms.ToTensor(),
         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        transforms.RandomErasing()
     ]),
     'val': transforms.Compose([
         transforms.Resize((224, 224)),
@@ -83,7 +65,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=100):
         # 각 에폭(epoch)은 학습 단계와 검증 단계를 갖습니다.
         for phase in ['train', 'val']:
             if phase == 'train':
-                model.train() # 모델을 학습 모드로 설정
+                model.train()  # 모델을 학습 모드로 설정
             else:
                 model.eval()   # 모델을 평가 모드로 설정
 
@@ -103,18 +85,13 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=100):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
-                    c = (preds ==labels).squeeze()
+
                     # 학습 단계인 경우 역전파 + 최적화
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
 
                 # 통계
-                for i in range(4):
-                    label = labels[i]
-                    class_correct[label] += c[i].item()
-                    class_total[label] +=1
-
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
             if phase == 'train':
@@ -126,12 +103,6 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=100):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            for i in range(186):
-                try:
-                    print('Accuracy %2d %% of %5s : %2d %%' % (i,class_name[i], 100 * class_correct[i] / class_total[i]))
-                except:
-                    break
-
             # 모델을 깊은 복사(deep copy)함
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
@@ -141,8 +112,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=100):
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
-    for i in range(186):
-        print('Accuracy of %5s : %2d %%' % (class_name[i], 100 * class_correct[i] / class_total[i]))
+
     # 가장 나은 모델 가중치를 불러옴
     model.load_state_dict(best_model_wts)
     return model
@@ -158,12 +128,15 @@ if __name__ == '__main__':
     model_conv = model_conv.to(device)
     criterion = nn.CrossEntropyLoss()
     #optimizer_ft = optim.Adam(model_conv.parameters(), lr=0.01) #lr=0.001, momentum=0.9
-    optimizer_ft = AdamP(model_conv.parameters(), lr=0.01)
+    optimizer_ft = AdamP(model_conv.parameters(), lr=0.01) #lr=0.001, momentum=0.9
     # 7 에폭마다 0.1씩 학습율 감소
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-    train_model(model_conv, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=50)
 
+    # net = Net() 
+    # net = net.cuda()
+    # net = torch.nn.DataParallel(net)
+    # cudnn.benchmark = False  
+    train_model(model_conv, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=100)
 
-
-    PATH = './20200622_2.pth'
+    PATH = './test0620.pth'   
     torch.save(model_conv.state_dict(), PATH)
